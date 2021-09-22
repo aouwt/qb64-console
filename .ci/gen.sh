7fb57ffd3f4a1bdc64bf2d17dca8e80c0b0b318f
all-other-usernames-were-taken <74026992+all-other-usernames-were-taken@users.noreply.github.com>
2021-09-22 17:03:20 -0400

    oh come on

--- .ci/gen.sh
+++ .ci/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone https://github.com/QB64Team/QB64 --depth=1 --branch=${{ steps.extract_branch.outputs.branch }}; cd QB64
+git clone https://github.com/QB64Team/QB64 --depth=1 --branch=$1; cd QB64
 # QB64 errors (for some reason) when you include an empty file when compiling via CLI
 echo "" > ./source/ide/ide_methods.bas
 echo "" > ./source/ide/ide_global.bas
