504def3fb3e6fa75d6e446470ff48912cacd0e2a
Kit <74026992+aouwt@users.noreply.github.com>
2021-10-31 10:10:27 -0700

    Update gen.sh

--- .ci/gen.sh
+++ .ci/gen.sh
@@ -3,11 +3,22 @@ git clone https://github.com/QB64Team/QB64 --depth=1 --branch=$1; cd QB64
 # QB64 errors (for some reason) when you include an empty file when compiling via CLI
 echo "" > ./source/ide/ide_methods.bas
 echo "" > ./source/ide/ide_global.bas
+
+echo "" > /tmp/qb64.bas
+for L in `cat ./source/qb64.bas`; do
+  if [ "$L" = "$CONSOLE" ]; then
+    echo "$CONSOLE:ONLY" >> /tmp/qb64.bas
+  else
+    echo "$L" >> /tmp/qb64.bas
+  fi
+done
+mv /tmp/qb64.bas ./source/qb64.bas
+
 ./.ci/bootstrap.sh lnx
 ./.ci/compile.sh
 rm -rf ./.github ./.ci ./.git
 for F in .; do
- cp -rf "$F" "../$F"
+  cp -rf "$F" "../$F"
 done
 
 cd ..
