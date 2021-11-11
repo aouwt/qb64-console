96bfc7527a0363a3dfcce634ef00acf633d7c12e
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-11 05:12:43 -0800

    Update gen.sh

--- .ci/gen.sh
+++ .ci/gen.sh
@@ -4,15 +4,7 @@ git clone https://github.com/QB64Team/QB64 --depth=1 --branch=$1; cd QB64
 echo "" > ./source/ide/ide_methods.bas
 echo "" > ./source/ide/ide_global.bas
 
-echo "" > /tmp/qb64.bas
-for L in `cat ./source/qb64.bas`; do
-  if [ "$L" = "$CONSOLE" ]; then
-    echo "$CONSOLE:ONLY" >> /tmp/qb64.bas
-  else
-    echo "$L" >> /tmp/qb64.bas
-  fi
-done
-mv /tmp/qb64.bas ./source/qb64.bas
+sed -i 's/\n$CONSOLE/\n$CONSOLE:ONLY/g' ./source/qb64.bas
 
 ./.ci/bootstrap.sh lnx
 ./.ci/compile.sh
